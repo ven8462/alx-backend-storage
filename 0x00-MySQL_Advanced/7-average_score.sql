201e4c4ca1e42df9011718583c3200e448026b26
lavender <lavenderanyango9@gmail.com>
2024-01-18 20:43:23 +0300

    Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,16 +1,16 @@
 --  SQL script that creates a stored procedure ComputeAverageScoreForUser
 -- that computes and store the average score for a student
 
-DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+drop procedure IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$ ;
+CREATE PROCEDURE ComputeAverageScoreForUser(
+	IN user_id INT
+)
 BEGIN
-    DECLARE avg_score DECIMAL(10, 2);
-    
-    SELECT AVG(score) INTO avg_score
-    FROM scores
-    WHERE user_id = user_id;
-    
-    INSERT INTO average_scores (user_id, average_score)
-    VALUES (user_id, avg_score);
-END$$
+	UPDATE users
+   	SET average_score=(SELECT AVG(score) FROM corrections
+			     WHERE corrections.user_id=user_id)
+	WHERE id=user_id;
+
+END;$$
 DELIMITER ;
