5f1127f8377f106115badaac7de6cb69bd86040a
lavender <lavenderanyango9@gmail.com>
2024-01-18 22:02:01 +0300

    index names

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,3 @@
 
 -- Create the index idx_name_first on the table names and the first letter of name
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));
+CREATE INDEX idx_name_first ON names(name(1))
