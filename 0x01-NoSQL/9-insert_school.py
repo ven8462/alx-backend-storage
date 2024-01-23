700c3ab472d583cf0eb8365a332ea98c1d2222d5
lavender <lavenderanyango9@gmail.com>
2024-01-23 21:37:41 +0300

    Insert a document in Python

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+# !/usr/bin/env python3
+""" pymongo + NoSQL module """
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''inserts a new document in a collection based on kwargs'''
+    return mongo_collection.insert_one(kwargs).inserted_id
