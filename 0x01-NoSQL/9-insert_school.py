b53c1ed0b67c9910c1c5d654e34f901752a587c5
lavender <lavenderanyango9@gmail.com>
2024-01-23 21:45:56 +0300

    Insert a document in Python

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,7 +1,23 @@
-# !/usr/bin/env python3
-""" pymongo + NoSQL module """
+#!/usr/bin/env python3
+""" pymongo + NoSQL module"""
+from pymongo import MongoClient
+list_all = __import__('8-all').list_all
 
 
 def insert_school(mongo_collection, **kwargs):
-    '''inserts a new document in a collection based on kwargs'''
+    """ inserts a new document in a collection based on kwargs """
     return mongo_collection.insert_one(kwargs).inserted_id
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    school_collection = client.my_db.school
+    new_school_id = insert_school(
+        school_collection, name="UCSF", address="505 Parnassus Ave")
+    print("New school created: {}".format(new_school_id))
+
+    schools = list_all(school_collection)
+    for school in schools:
+        print(
+            "[{}] {} {}".format(school.get('_id'),
+                                school.get('name'), school.get('address', "")))
