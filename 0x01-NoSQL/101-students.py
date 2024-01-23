18586b618bb107e38b17470bc768984b4195761f
lavender <lavenderanyango9@gmail.com>
2024-01-23 22:20:09 +0300

    list of all students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Module for 101-students.
+"""
+
+
+def top_students(mongo_collection):
+    """
+    Returns all students sorted by average score
+    """
+    return mongo_collection.aggregate([
+        {
+            "$project":
+            {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort":
+            {
+                "averageScore": -1
+            }
+        }
+    ])
